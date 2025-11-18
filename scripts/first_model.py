43e9e9eb3587ff0238eb4bac1804c459d0791a31
Cave <a@a.com>
2025-11-18 21:22:45 +0100

    first commit

---
+++ scripts/first_model.py
@@ -0,0 +1,103 @@
+"""
+Random Forest model
+"""
+import pandas as pd
+import numpy as np
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+import pickle
+
+import time
+#############################################
+
+filename = '../data/fitted_dataset.csv'
+
+df = pd.read_csv(filename, delimiter=',')
+
+#############################################
+
+###  PARAMETERS  ###
+
+TEST_SIZE = 0.2
+
+RANDOM_SEED = 2020
+
+TREES_NUMBER = 200
+
+MAX_DEPTH = None
+
+#############################################
+
+###  Split dataset  ###
+
+# Target variable
+y = df.iloc[:, 0]  # Price (in Euro)
+
+# Features
+X = df.iloc[:, 1:]
+
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=TEST_SIZE,
+                                                    random_state=RANDOM_SEED)
+
+#############################################
+start_time = time.perf_counter()
+################################
+
+###  Model training   ###
+
+model = RandomForestRegressor(n_estimators=TREES_NUMBER,
+                              max_depth=MAX_DEPTH,
+                              min_samples_leaf=MIN_LEAFS_NUM
+                              random_state=RANDOM_SEED)
+
+model.fit(X_train, y_train)
+
+##############################
+end_time = time.perf_counter()
+#############################################
+elapsed_time = end_time - start_time
+print(f"\nTimer: {elapsed_time:.0f} sec\n")
+#############################################
+
+###  Prediction and evaluation  ###
+
+y_pred = model.predict(X_test)
+
+# Root Mean Squared Error (RMSE)
+
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))  # RMSE, in Euros
+
+print(f'RMSE = {rmse:.0f}')
+
+# Mean Absolute Error (MAE)
+
+mae = mean_absolute_error(y_test, y_pred)
+
+print(f'MAE = {mae:.0f}')
+
+# R2 (coefficient of determination)
+
+r2 = r2_score(y_test, y_pred)
+
+print(f'R2 = {r2:.3f}')
+
+#############################################
+
+SAVE_MODEL = 0
+
+models_file_name = '../models/random_forest_model.pkl'
+
+if(SAVE_MODEL):
+    with open(models_file_name, 'wb') as f:
+        pickle.dump(model, f)
+
+#############################################
+
+print('\nTask completed!')
+
+#############################################
---
+++ scripts/prepare_dataset.py
@@ -0,0 +1,70 @@
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder
+
+################################################
+
+filename = '../data/original_dataset_v3.csv'
+
+df = pd.read_csv(filename, delimiter=',')
+
+##################################################################
+
+df.drop(df.columns[0], axis=1, inplace=True)  # Unnamed
+
+df.drop(columns=['url', 'locality', 'sale_type', 'log_price_m2', 'cluster'],
+        inplace=True)
+
+df.drop(columns=['property_id'], inplace=True)  # Delete index
+
+new_order = [   'price',
+                'postal_code',
+                'rooms',
+                'area',
+                'state',
+                'has_open_fire',
+                'property_type',
+                'property_subtype',
+                'facades_number',
+                'is_furnished',
+                'has_terrace',
+                'has_garden',
+                'has_swimming_pool',
+                'has_equipped_kitchen',
+                'is_luxurious']
+                
+df = df[new_order]
+
+##################################################################
+
+df['state'] = LabelEncoder().fit_transform(df['state'].fillna('None'))
+
+df['property_type'] = LabelEncoder().fit_transform(df['property_type'])
+df['property_subtype'] = LabelEncoder().fit_transform(df['property_subtype'])
+
+df['has_swimming_pool'] = LabelEncoder().fit_transform(df['has_swimming_pool'])
+df['has_equipped_kitchen'] = LabelEncoder().fit_transform(df['has_equipped_kitchen'])
+
+df['is_luxurious'] = df['is_luxurious'].astype(int)
+
+df.fillna(-1, inplace=True)
+
+df = df.astype({col: 'int64' for col in df.select_dtypes('float64').columns})
+
+##################################################################
+
+if (0):
+    
+    print(df.dtypes)
+
+##################################################################
+
+if (1):
+
+    filename = '../data/fitted_dataset.csv'
+
+    df.to_csv(filename, sep=',', index=False)
+
+##################################################################
+
+print('The job have done')
